3c343ab286b7d09a42f2d81e663fd170c45d5af5
victor <vitimm144@gmail.com>
2015-01-05 15:04:15 -0200

    Ceps já cadastrados nao aparecem mais para serem selecionados

--- ajax-cep.php
+++ ajax-cep.php
@@ -1,17 +1,20 @@
 <?php
     include_once ("Connections/config.php");
-    $conexao = mysqli_connect("$hostname_config", "$username_config", "$password_config")
-                or die(mysql_error());
-    $db = mysql_select_db("$database_config")
-                or die(mysql_error());
+    $conexao = mysqli_connect("$hostname_config", "$username_config", "$password_config", "$database_config");
+    
     $bairro = $_POST['tipo'];
     $sql = "SELECT id, cep, tp_logradouro, logradouro FROM rj WHERE bairro='$bairro'";
-    $query = mysql_query($sql);
+    $query = mysqli_query($conexao, $sql);
 
-    while ($cep = mysql_fetch_array($query)) {
-
+    while ($cep = mysqli_fetch_array($query)) {
+        //filtrando ceps já cadastrados
+        $filter_sql = "SELECT id from usuario_cep where id_cep=".$cep['id'].";";
+        $filter = mysqli_query($conexao, $filter_sql);
+        if($filter->num_rows != 0){
+            continue;
+        }
+        //############################
         echo '<input type="checkbox" name="cep[]" value="'.$cep['id'].'" />'.$cep['cep'].' - '.$cep['tp_logradouro']. ' - '.$cep['logradouro']. '<br/>';
     }
-//    mysql_close($conexao);
 
 
